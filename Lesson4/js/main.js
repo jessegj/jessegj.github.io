a8dc7ad4cc207bae68195aeab3fbf7ca6ef7dd46
jessegj <jen18055@byui.edu>
2020-05-15 18:32:41 -0600

    toggle

--- Lesson4/js/main.js
+++ Lesson4/js/main.js
@@ -2,3 +2,8 @@ let date = new Date(document.lastModified);
 let time = date.toLocaleString("en-US");
 
 document.getElementById("lastTime").textContent = time;
+
+function toggleMenu() {
+    console.log(document.getElementById("primaryNav").classList);
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
