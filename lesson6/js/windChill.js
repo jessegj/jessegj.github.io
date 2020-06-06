b3a4e1a30299de175ecaad9449f666ce8aa1580b
jessegj <jen18055@byui.edu>
2020-06-05 18:03:06 -0600

    windchill

--- lesson6/js/windChill.js
+++ lesson6/js/windChill.js
@@ -1,10 +1,10 @@
 function windChill ()
 {
-    let temp = document.getElementById('temp').innerHTML;
-    let wind = document.getElementById('wind').innerHTML;
+    let temp = (parseFloat(document.getElementById('temp').innerHTML));
+    let wind = (parseFloat(document.getElementById('wind').innerHTML));
     let chill = "";
 
     chill = ((35.74 + (0.6215 * temp)) - (35.75 * Math.pow(wind, 0.16)) + ((0.4275 * temp) * Math.pow(wind, 0.16)));
 
-    document.getElementById('chill').innerHTML = chill;
+    document.getElementById('chill').innerHTML = chill.toFixed(0);
 }
