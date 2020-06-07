a31f9a0b1a622342ed59e5f3971b6a215630b667
jessegj <jen18055@byui.edu>
2020-06-06 22:45:25 -0600

    lotsa changes

--- lesson7/js/lazyloader.js
+++ lesson7/js/lazyloader.js
@@ -1,37 +1,29 @@
-//get all imgs with data src attribute
-const imagesToLoad = document.querySelectorAll("img[data-src]");
+//get all the images from data-src
+const images = document.SelectorAll("img[data-src");
 
-// optional parameters being set for the IntersectionalObserver
-const imgOptions = {
-threshold: 1,
-rootMarging: "0px 0px 50px 0px"
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if(!src) {
+        return;
+    }
+
+    img.src = src;
+}
+
+const imgOptions ={
 };
 
-const loadImages = (image) => {
-image.setAttribute('src', image.getAttribute('data-src'));
-image.onload = () => {image.removeAttribute('data-src');};
-};
-
-// first check to see if intersection Observer is supported
-if('IntersectionObserver' in window) {
-const imgObserver = new IntersectionObserver((items, observer) => {
-    items.forEach((item) => {
-        if(item.isIntersecting) {
-            loadImages(item.target);
-            observer.unobserve(item.target);
-        } 
-    });
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        }   else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
 }, imgOptions);
 
-// loop through each img an check status and load if necessary
-imagesToLoad.forEach((img) => {
-    imgObserver.observe(img);
-});
-}
-else {// just load All immages if not supported
-    imagesToLoad.forEach((img) => {
-        loadImages(img);
-    })
-
-
-}
+images.forEach(image => {
+    imgObserver.observe(image);
+})
