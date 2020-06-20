f6bc79aa23e4d41b23288abce78296e7f9fd33f9
jessegj <jen18055@byui.edu>
2020-06-20 17:39:57 -0600

    --- Auto-Git Commit ---

---
+++ lesson9/js/homepage.js
@@ -0,0 +1,36 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(jsonObject) {
+        console.table(jsonObject); // temporary checking for valid response and data parsing
+        const prophets = jsonObject['towns'];
+
+        for (let i = 0; i < towns.length; i++) {
+
+
+
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let bDay = document.createElement('p');
+            let bPlace = document.createElement('p');
+            let image = document.createElement('img');
+            let alt = document.createElement('alt');
+
+            alt.setAttribute('alt', );
+            image.setAttribute('src', prophets[i].imageurl);
+            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            bDay.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+            bPlace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+
+            card.appendChild(h2);
+            card.appendChild(alt);
+            card.appendChild(bDay);
+            card.appendChild(bPlace);
+            card.appendChild(image);
+            document.querySelector('div.cards').appendChild(card);
+        }
+
+    });
