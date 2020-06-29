dcf3eb3e4dd9aa4c0f9d880b5a60e6ca1d991e4c
jessegj <jen18055@byui.edu>
2020-06-29 15:55:56 -0600

    --- Auto-Git Commit ---

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,7 +1,16 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=ec1da9735ed79464d9674d9bfcb53a59';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=ec1da9735ed79464d9674d9bfcb53a59';
 
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject);
+        console.log(jsObject); <
+        span id = "current-temp" > < /span>
+            //gets the current temperature from the API
+        document.getElementById('current-temp').textContent = jsObject.main.temp;
+
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
+        const desc = jsObject.weather[0].description; // note how we reference the weather array
+        document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
+        document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
+        document.getElementById('icon').setAttribute('alt', desc);
     });
