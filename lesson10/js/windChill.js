28019ec87be68d82f921c2bc343150a023080c79
jessegj <jen18055@byui.edu>
2020-06-29 18:56:46 -0600

    --- Auto-Git Commit ---

--- lesson10/js/windChill.js
+++ lesson10/js/windChill.js
@@ -1,5 +1,31 @@
-function windChill ()
-{
+//weather summary API
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=ec1da9735ed79464d9674d9bfcb53a59&units=imperial';
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        const currentCondition = document.querySelector('#condition')
+        const temp = document.querySelector('#temp');
+        const humid = document.querySelector('#humid');
+        const wind = document.querySelector('#wind');
+        const icon = document.querySelector('#icon');
+
+        currentCondition.textContent = jsObject.weather[0].description;
+        temp.textContent = jsObject.main.temp.toFixed(0);
+        humid.textContent = jsObject.main.humidity.toFixed(0);
+        wind.textContent = jsObject.wind.speed.toFixed(0);
+
+        //const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+        //const desc = jsObject.weather[0].descritpion;
+
+        //icon.setAttribute('src', imagesrc);
+        //icon.setAttribute('alt', desc);
+
+    });
+
+function windChill() {
     let temp = (parseFloat(document.getElementById('temp').innerHTML));
     let wind = (parseFloat(document.getElementById('wind').innerHTML));
     let chill = "";
