d273efa56deb5193025da38be190c4e24c43568a
jessegj <jen18055@byui.edu>
2020-07-21 18:39:22 -0600

    cozumel pctires

---
+++ scootsproject/js/lazyloader.js
@@ -0,0 +1,33 @@
+//get all the images on page from data-src
+const imagesToLoad = document.querySelectorAll('img[data-src]')
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');};
+};
+
+const imgOptions = {
+    threshold: 0.5
+}
+
+if('instersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    //load image if necessary
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+}
+//load img not supported
+else {
+    imagesToLoad.forEach( (img)=> {
+        loadImages(img);
+    });
+}
